a443428279d93a2d0ecae96ad0f247defc1ab694
Austin <mathfreak65@gmail.com>
2020-07-27 00:19:52 +0000

    Upgrading redis and rq.
    Both experienced major version bumps which broke backwards compatibility.
    Unsurprisingly, all the parts that impacted this app were in rq, since
    it interacts directly with rq much more than redis.

--- routes18xxweb/calculator.py
+++ routes18xxweb/calculator.py
@@ -5,8 +5,6 @@ import traceback
 import redis
 from rq import Worker, Queue, Connection
 from rq.job import Job
-from rq.handlers import move_to_failed_queue
-from rq.queue import get_failed_queue
 
 listen = ['high', 'default', 'low']
 
@@ -15,14 +13,12 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis_conn = redis.from_url(redis_url)
 
 def handle_exception(job, exc_type, exc_value, tb_obj):
-    failed_queue = get_failed_queue(redis_conn, job.__class__)
-
     exc_info_str = json.dumps({
         "message": str(exc_value),
         "traceback": Worker._get_safe_exception_string(traceback.format_exception(exc_type, exc_value, tb_obj))
     })
 
-    failed_queue.quarantine(job, exc_info=exc_info_str)
+    job.failed_job_registry.add(job, exc_string=exc_info_str)
 
     return False
 
